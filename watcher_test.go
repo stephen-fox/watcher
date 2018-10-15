4d34bbeebe40b808b64a4276826ba7afd551d037
Stephen Fox <stephen.j.fox.jr@gmail.com>
2018-10-15 18:32:20 -0400

    Added tests for ScanFilesInSubdirectories.
    Also created directory specifically for tests data. This way, the
    tests do not rely on the source code being laid out a certain way.

--- watcher_test.go
+++ watcher_test.go
@@ -7,6 +7,10 @@ import (
 	"time"
 )
 
+const (
+	searchFileExt = ".txt"
+)
+
 func TestConfig_IsValid(t *testing.T) {
 	emptyErr := Config{}.IsValid()
 	if emptyErr == nil {
@@ -119,16 +123,11 @@ func TestNewWatcher(t *testing.T) {
 	}
 }
 
-func TestDefaultWatcher_Start(t *testing.T) {
-	current, err := os.Getwd()
-	if err != nil {
-		t.Error(err.Error())
-	}
-
+func TestDefaultWatcherScanFilesInDirectory_Start(t *testing.T) {
 	config := Config{
 		RefreshDelay: 1 * time.Second,
-		RootDirPath:  current,
-		FileSuffix:   ".go",
+		RootDirPath:  testDataDirPath(),
+		FileSuffix:   searchFileExt,
 		Changes:      make(chan Changes),
 		ScanFunc:     ScanFilesInDirectory,
 	}
@@ -146,9 +145,8 @@ func TestDefaultWatcher_Start(t *testing.T) {
 	}
 
 	exp := []string{
-		"doc.go",
-		"watcher.go",
-		"watcher_test.go",
+		"file1.txt",
+		"file2.txt",
 	}
 
 	if len(changes.UpdatedFilePaths) == 0 {
@@ -167,16 +165,11 @@ func TestDefaultWatcher_Start(t *testing.T) {
 	}
 }
 
-func TestDefaultWatcher_StartMultipleTimes(t *testing.T) {
-	current, err := os.Getwd()
-	if err != nil {
-		t.Error(err.Error())
-	}
-
+func TestDefaultWatcherScanFilesInDirectory_StartMultipleTimes(t *testing.T) {
 	config := Config{
 		RefreshDelay: 1 * time.Second,
-		RootDirPath:  current,
-		FileSuffix:   ".go",
+		RootDirPath:  testDataDirPath(),
+		FileSuffix:   searchFileExt,
 		Changes:      make(chan Changes),
 		ScanFunc:     ScanFilesInDirectory,
 	}
@@ -211,16 +204,11 @@ func TestDefaultWatcher_StartMultipleTimes(t *testing.T) {
 	}
 }
 
-func TestDefaultWatcher_Stop(t *testing.T) {
-	current, err := os.Getwd()
-	if err != nil {
-		t.Error(err.Error())
-	}
-
+func TestDefaultWatcherScanFilesInDirectory_Stop(t *testing.T) {
 	config := Config{
 		RefreshDelay: 1 * time.Second,
-		RootDirPath:  current,
-		FileSuffix:   ".go",
+		RootDirPath:  testDataDirPath(),
+		FileSuffix:   searchFileExt,
 		Changes:      make(chan Changes),
 		ScanFunc:     ScanFilesInDirectory,
 	}
@@ -252,16 +240,11 @@ func TestDefaultWatcher_Stop(t *testing.T) {
 	}
 }
 
-func TestDefaultWatcher_StopWithoutStart(t *testing.T) {
-	current, err := os.Getwd()
-	if err != nil {
-		t.Error(err.Error())
-	}
-
+func TestDefaultWatcherScanFilesInDirectory_StopWithoutStart(t *testing.T) {
 	config := Config{
 		RefreshDelay: 1 * time.Second,
-		RootDirPath:  current,
-		FileSuffix:   ".go",
+		RootDirPath:  testDataDirPath(),
+		FileSuffix:   searchFileExt,
 		Changes:      make(chan Changes),
 		ScanFunc:     ScanFilesInDirectory,
 	}
@@ -291,16 +274,11 @@ func TestDefaultWatcher_StopWithoutStart(t *testing.T) {
 	}
 }
 
-func TestDefaultWatcher_StartStopStartStop(t *testing.T) {
-	current, err := os.Getwd()
-	if err != nil {
-		t.Error(err.Error())
-	}
-
+func TestDefaultWatcherScanFilesInDirectory_StartStopStartStop(t *testing.T) {
 	config := Config{
 		RefreshDelay: 1 * time.Second,
-		RootDirPath:  current,
-		FileSuffix:   ".go",
+		RootDirPath:  testDataDirPath(),
+		FileSuffix:   searchFileExt,
 		Changes:      make(chan Changes),
 		ScanFunc:     ScanFilesInDirectory,
 	}
@@ -378,16 +356,11 @@ func TestDefaultWatcher_StartStopStartStop(t *testing.T) {
 	}
 }
 
-func TestDefaultWatcher_StopMultipleTimes(t *testing.T) {
-	current, err := os.Getwd()
-	if err != nil {
-		t.Error(err.Error())
-	}
-
+func TestDefaultWatcherScanFilesInDirectory_StopMultipleTimes(t *testing.T) {
 	config := Config{
 		RefreshDelay: 1 * time.Second,
-		RootDirPath:  current,
-		FileSuffix:   ".go",
+		RootDirPath:  testDataDirPath(),
+		FileSuffix:   searchFileExt,
 		Changes:      make(chan Changes),
 		ScanFunc:     ScanFilesInDirectory,
 	}
@@ -423,16 +396,11 @@ func TestDefaultWatcher_StopMultipleTimes(t *testing.T) {
 	}
 }
 
-func TestDefaultWatcher_Destroy(t *testing.T) {
-	current, err := os.Getwd()
-	if err != nil {
-		t.Error(err.Error())
-	}
-
+func TestDefaultWatcherScanFilesInDirectory_Destroy(t *testing.T) {
 	config := Config{
 		RefreshDelay: 1 * time.Second,
-		RootDirPath:  current,
-		FileSuffix:   ".go",
+		RootDirPath:  testDataDirPath(),
+		FileSuffix:   searchFileExt,
 		Changes:      make(chan Changes),
 		ScanFunc:     ScanFilesInDirectory,
 	}
@@ -477,16 +445,11 @@ func TestDefaultWatcher_Destroy(t *testing.T) {
 	t.Error("Changes channel is still open after destroy")
 }
 
-func TestDefaultWatcher_DestroyMultipleTimes(t *testing.T) {
-	current, err := os.Getwd()
-	if err != nil {
-		t.Error(err.Error())
-	}
-
+func TestDefaultWatcherScanFilesInDirectory_DestroyMultipleTimes(t *testing.T) {
 	config := Config{
 		RefreshDelay: 1 * time.Second,
-		RootDirPath:  current,
-		FileSuffix:   ".go",
+		RootDirPath:  testDataDirPath(),
+		FileSuffix:   searchFileExt,
 		Changes:      make(chan Changes),
 		ScanFunc:     ScanFilesInDirectory,
 	}
@@ -533,4 +496,400 @@ func TestDefaultWatcher_DestroyMultipleTimes(t *testing.T) {
 	}
 
 	t.Error("Changes channel is still open after destroy")
+}
+
+func TestDefaultWatcherScanFilesInSubdirectories_Start(t *testing.T) {
+	config := Config{
+		RefreshDelay: 1 * time.Second,
+		RootDirPath:  testDataDirPath(),
+		FileSuffix:   searchFileExt,
+		Changes:      make(chan Changes),
+		ScanFunc:     ScanFilesInSubdirectories,
+	}
+	w, err := NewWatcher(config)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	defer w.Stop()
+
+	w.Start()
+
+	changes := <-config.Changes
+	if changes.IsErr() {
+		t.Error(changes.Err)
+	}
+
+	exp := []string{
+		"subdirfile1.txt",
+		"subdirfile2.txt",
+	}
+
+	if len(changes.UpdatedFilePaths) == 0 {
+		t.Error("Updated file paths should not be empty")
+	}
+
+	OUTER:
+	for _, filePath := range changes.UpdatedFilePaths {
+		for _, e := range exp {
+			if path.Base(filePath) == e {
+				continue OUTER
+			}
+		}
+
+		t.Error("Got unexpected file path -", filePath)
+	}
+}
+
+func TestDefaultWatcherScanFilesInSubdirectories_StartMultipleTimes(t *testing.T) {
+	config := Config{
+		RefreshDelay: 1 * time.Second,
+		RootDirPath:  testDataDirPath(),
+		FileSuffix:   searchFileExt,
+		Changes:      make(chan Changes),
+		ScanFunc:     ScanFilesInSubdirectories,
+	}
+	w, err := NewWatcher(config)
+	if err != nil {
+		t.Error("Valid config generated an error -", err.Error())
+	}
+	defer w.Stop()
+
+	w.Start()
+	w.Start()
+	w.Start()
+	w.Start()
+	w.Start()
+
+	ticker := time.NewTicker(config.RefreshDelay * 2)
+	defer ticker.Stop()
+
+	var count int
+	OUTER:
+	for {
+		select {
+		case <-config.Changes:
+			count++
+		case <-ticker.C:
+			break OUTER
+		}
+	}
+
+	if count != 1 {
+		t.Error("Did not receive expected changes -", count)
+	}
+}
+
+func TestDefaultWatcherScanFilesInSubdirectories_Stop(t *testing.T) {
+	config := Config{
+		RefreshDelay: 1 * time.Second,
+		RootDirPath:  testDataDirPath(),
+		FileSuffix:   searchFileExt,
+		Changes:      make(chan Changes),
+		ScanFunc:     ScanFilesInSubdirectories,
+	}
+	w, err := NewWatcher(config)
+	if err != nil {
+		t.Error("Valid config generated an error -", err.Error())
+	}
+
+	w.Start()
+
+	w.Stop()
+
+	ticker := time.NewTicker(config.RefreshDelay * 2)
+	defer ticker.Stop()
+
+	var count int
+	OUTER:
+	for {
+		select {
+		case <-ticker.C:
+			break OUTER
+		case <-config.Changes:
+			count++
+		}
+	}
+
+	if count > 0 {
+		t.Error("Results were produced after stopping -", count)
+	}
+}
+
+func TestDefaultWatcherScanFilesInSubdirectories_StopWithoutStart(t *testing.T) {
+	config := Config{
+		RefreshDelay: 1 * time.Second,
+		RootDirPath:  testDataDirPath(),
+		FileSuffix:   searchFileExt,
+		Changes:      make(chan Changes),
+		ScanFunc:     ScanFilesInSubdirectories,
+	}
+	w, err := NewWatcher(config)
+	if err != nil {
+		t.Error("Valid config generated an error -", err.Error())
+	}
+
+	w.Stop()
+
+	ticker := time.NewTicker(config.RefreshDelay * 2)
+	defer ticker.Stop()
+
+	var count int
+	OUTER:
+	for {
+		select {
+		case <-ticker.C:
+			break OUTER
+		case <-config.Changes:
+			count++
+		}
+	}
+
+	if count > 0 {
+		t.Error("Results were produced after stopping -", count)
+	}
+}
+
+func TestDefaultWatcherScanFilesInSubdirectories_StartStopStartStop(t *testing.T) {
+	config := Config{
+		RefreshDelay: 1 * time.Second,
+		RootDirPath:  testDataDirPath(),
+		FileSuffix:   searchFileExt,
+		Changes:      make(chan Changes),
+		ScanFunc:     ScanFilesInSubdirectories,
+	}
+	w, err := NewWatcher(config)
+	if err != nil {
+		t.Error("Valid config generated an error -", err.Error())
+	}
+
+	w.Start()
+	ticker := time.NewTicker(config.RefreshDelay * 2)
+	var count int
+	OUTER:
+	for {
+		select {
+		case <-ticker.C:
+			break OUTER
+		case <-config.Changes:
+			count++
+		}
+	}
+	ticker.Stop()
+	if count != 1 {
+		t.Error("Did not get expected number of results -", count)
+	}
+
+	w.Stop()
+	ticker = time.NewTicker(config.RefreshDelay * 2)
+	count = 0
+	OUTER2:
+	for {
+		select {
+		case <-ticker.C:
+			break OUTER2
+		case <-config.Changes:
+			count++
+		}
+	}
+	ticker.Stop()
+	if count > 0 {
+		t.Error("Results were produced after stopping -", count)
+	}
+
+	w.Start()
+	ticker = time.NewTicker(config.RefreshDelay * 2)
+	count = 0
+	OUTER3:
+	for {
+		select {
+		case <-ticker.C:
+			break OUTER3
+		case <-config.Changes:
+			count++
+		}
+	}
+	ticker.Stop()
+	if count != 1 {
+		t.Error("Did not get 1 result after starting -", count)
+	}
+
+	w.Stop()
+	ticker = time.NewTicker(config.RefreshDelay * 2)
+	count = 0
+	OUTER4:
+	for {
+		select {
+		case <-ticker.C:
+			break OUTER4
+		case <-config.Changes:
+			count++
+		}
+	}
+	ticker.Stop()
+	if count > 0 {
+		t.Error("Results were produced after stopping -", count)
+	}
+}
+
+func TestDefaultWatcherScanFilesInSubdirectories_StopMultipleTimes(t *testing.T) {
+	current := testDataDirPath()
+
+	config := Config{
+		RefreshDelay: 1 * time.Second,
+		RootDirPath:  current,
+		FileSuffix:   searchFileExt,
+		Changes:      make(chan Changes),
+		ScanFunc:     ScanFilesInSubdirectories,
+	}
+	w, err := NewWatcher(config)
+	if err != nil {
+		t.Error("Valid config generated an error -", err.Error())
+	}
+
+	w.Start()
+
+	w.Stop()
+	w.Stop()
+	w.Stop()
+	w.Stop()
+	w.Stop()
+
+	ticker := time.NewTicker(config.RefreshDelay * 2)
+	defer ticker.Stop()
+
+	var count int
+	OUTER:
+	for {
+		select {
+		case <-ticker.C:
+			break OUTER
+		case <-config.Changes:
+			count++
+		}
+	}
+
+	if count > 0 {
+		t.Error("More than one result was produced after stopping multiple times -", count)
+	}
+}
+
+func TestDefaultWatcherScanFilesInSubdirectories_Destroy(t *testing.T) {
+	current := testDataDirPath()
+
+	config := Config{
+		RefreshDelay: 1 * time.Second,
+		RootDirPath:  current,
+		FileSuffix:   searchFileExt,
+		Changes:      make(chan Changes),
+		ScanFunc:     ScanFilesInSubdirectories,
+	}
+	w, err := NewWatcher(config)
+	if err != nil {
+		t.Error("Valid config generated an error -", err.Error())
+	}
+
+	w.Start()
+
+	w.Destroy()
+
+	ticker := time.NewTicker(config.RefreshDelay * 2)
+
+	var count int
+
+	OUTER:
+	for {
+		select {
+		case <-ticker.C:
+			break OUTER
+		case _, ok := <-config.Changes:
+			if ok {
+				count++
+			}
+			break OUTER
+		}
+	}
+
+	if count > 0 {
+		t.Error("More than one result was produced after destroying -", count)
+	}
+
+	select {
+	case _, ok := <-config.Changes:
+		if !ok {
+			return
+		}
+	default:
+	}
+
+	t.Error("Changes channel is still open after destroy")
+}
+
+func TestDefaultWatcherScanFilesInSubdirectories_DestroyMultipleTimes(t *testing.T) {
+	current := testDataDirPath()
+
+	config := Config{
+		RefreshDelay: 1 * time.Second,
+		RootDirPath:  current,
+		FileSuffix:   searchFileExt,
+		Changes:      make(chan Changes),
+		ScanFunc:     ScanFilesInSubdirectories,
+	}
+	w, err := NewWatcher(config)
+	if err != nil {
+		t.Error("Valid config generated an error -", err.Error())
+	}
+
+	w.Start()
+
+	w.Destroy()
+	w.Destroy()
+	w.Destroy()
+	w.Destroy()
+	w.Destroy()
+	w.Destroy()
+
+	ticker := time.NewTicker(config.RefreshDelay * 2)
+
+	var count int
+	OUTER:
+	for {
+		select {
+		case <-ticker.C:
+			break OUTER
+		case _, ok := <-config.Changes:
+			if ok {
+				count++
+			}
+			break OUTER
+		}
+	}
+
+	if count > 0 {
+		t.Error("More than one result was produced after destroying multple times -", count)
+	}
+
+	select {
+	case _, ok := <-config.Changes:
+		if !ok {
+			return
+		}
+	default:
+	}
+
+	t.Error("Changes channel is still open after destroy")
+}
+
+func testDataDirPath() string {
+	current, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	final := path.Join(current, ".testdata")
+	_, err = os.Stat(final)
+	if err != nil {
+		panic(err)
+	}
+
+	return final
 }
